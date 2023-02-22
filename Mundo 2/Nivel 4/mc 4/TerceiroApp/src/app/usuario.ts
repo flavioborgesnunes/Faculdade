2382faf18f546de4b4930192e5c15ca633d9a0e7
flavioborgesnunes <flavioborgesnunes@gmail.com>
2023-02-22 17:50:29 -0300

    Missão Pratica 4 - Final

--- Mundo 2/Nivel 4/mc 4/TerceiroApp/src/app/usuario.ts
+++ Mundo 2/Nivel 4/mc 4/TerceiroApp/src/app/usuario.ts
@@ -4,11 +4,11 @@ export class Usuario {
     public nome: string;
     public email: string;
     constructor(login: string = "", senha: string = "",
-    nome: string = "", email: string=""){
-    this.login = login;
-    this.senha = senha;
-    this.nome = nome;
-    this.email = email;
+        nome: string = "", email: string = "") {
+        this.login = login;
+        this.senha = senha;
+        this.nome = nome;
+        this.email = email;
     }
-   }
-   
+}
+
